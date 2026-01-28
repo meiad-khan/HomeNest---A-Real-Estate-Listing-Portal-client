f02ff5b39db44e4aa18be64880e81a38d765790c
meiad-khan <meiadkhan615@gmail.com>
2026-01-28 12:35:17 +0600

    Implemented pagination

--- src/Pages/AllProperties.jsx
+++ src/Pages/AllProperties.jsx
@@ -1,20 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Provider/AuthContext';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const AllProperties = () => {
 
   const { user, loading } = useContext(AuthContext);
-  if (loading) {
-    return (
-      <>
-        <div className="min-h-screen flex justify-center items-center">
-          <span className="text-8xl loading loading-bars loading-xl"></span>
-        </div>
-      </>
-    );
-  }
+  
  
-  const [property, setProperty] = useState('');
+  const [property, setProperty] = useState([]);
   const [totalProperty, setTotalProperty] = useState(0);
   const [totalPage, setTotalPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
@@ -28,14 +21,29 @@ const AllProperties = () => {
       .then(res => res.json())
       .then(data => {
         // console.log('all product hrere', data);
-        setProperty(data);
+        setProperty(data.result);
+        setTotalProperty(data.total);
+        const page = Math.ceil(data.total / limit);
+        setTotalPage(page);
       })
   }, [limit, sort, order, searchText, currentPage]);
 
-  console.log('checking', property);
+  // console.log('checking', property);
+  
+  const handleSort = (e) => {
+    console.log(e.target.value);
+  }
 
-
-
+  if (loading) {
+    return (
+      <>
+        <div className="min-h-screen flex justify-center items-center">
+          <span className="text-8xl loading loading-bars loading-xl"></span>
+        </div>
+      </>
+    );
+  }
+  
 
   return (
     <div className="max-w-7xl mx-auto lg:p-4 mt-15 border-2 mb-20 shadow-md border-red-700">
@@ -46,9 +54,7 @@ const AllProperties = () => {
       {/* search and sort here */}
       <div className="flex flex-wrap justify-between my-5">
         <div>
-          <h2 className="text-3xl font-inter">
-            ({property.length}) Properties
-          </h2>
+          <h2 className="text-3xl font-inter">({totalProperty}) Properties</h2>
         </div>
         {/* search */}
         <div>
@@ -75,7 +81,11 @@ const AllProperties = () => {
 
         {/* sort */}
         <div>
-          <select defaultValue="Sort" className="select select-md">
+          <select
+            onChange={handleSort}
+            defaultValue="Sort"
+            className="select select-md"
+          >
             <option disabled={true}>Sort</option>
             <option>Medium Apple</option>
             <option>Medium Orange</option>
@@ -112,7 +122,7 @@ const AllProperties = () => {
 
                 {/* description */}
                 <div>
-                  <p>{p.description}</p>
+                  <p className="font-inter">Posted By: {p.userName}</p>
                 </div>
 
                 {/* location + price  */}
@@ -131,12 +141,34 @@ const AllProperties = () => {
             </div>
           ))}
       </div>
-      <div className='flex flex-wrap justify-end gap-3.5 my-5'>
-        {
-          [...Array(5).keys()].map(i => (
-            <button className={`btn ${i == currentPage && "btn-primary"}`}>{i+1}</button>
-          ))
-        }
+      <div className="flex flex-wrap justify-end gap-3.5 my-5">
+        {currentPage > 0 && (
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            className="btn"
+          >
+            <FaArrowLeft />
+            Prev
+          </button>
+        )}
+        {[...Array(totalPage).keys()].map((i) => (
+          <button
+            onClick={() => setCurrentPage(i)}
+            key={i}
+            className={`btn ${i == currentPage && "btn-primary"}`}
+          >
+            {i + 1}
+          </button>
+        ))}
+        {currentPage < totalPage - 1 && (
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            className="btn"
+          >
+            Next
+            <FaArrowRight />
+          </button>
+        )}
       </div>
     </div>
   );
